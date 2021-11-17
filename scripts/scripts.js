1fc8f3f69156a4e50f58010d6dd94c681f215e4f
Creeper <35629436+wgqcreeper@users.noreply.github.com>
2021-11-17 23:41:34 +0800

    Delete scripts directory

--- scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-var links=[
- "东方铃奈庵@blog.npofsi.pro",
- "W3School@http://www.w3school.com.cn/"
-];
-links.forEach(function(v){
-		var linkp=document.getElementById("linkp");
-		linkp.innerHTML+="<a href=\""+v.split("@")[1]+"\" class=\"list-group-item\">"+v.split("@")[0]+"</a>";
-	});
